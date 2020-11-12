12693f33c3fdfb35258ac9f3e76862fd558a25bc
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-12 18:18:59 +0300

    Working with Flask Web Forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import CommentForm
 
 @app.route('/')
 @app.route('/home')
@@ -22,3 +23,14 @@ def portfolio():
 @app.route('/my-interests')
 def my_interests():
     return render_template('my_interests.html', title = 'My Interests')
+
+# -------------------Articles Begin Here-----------------------
+@app.route('/flask-webforms', methods = ['GET', 'POST'])
+def flask_webforms():
+    form = CommentForm()
+    if form.validate_on_submit():
+        flash('{}\'s comment is now live!'.format(form.username.data))
+        return redirect(url_for('flask_webforms'))
+    return render_template('flask_webforms.html', title = 'Flask Webforms', form = form)
+
+# -------------------Articles End Here-----------------------
