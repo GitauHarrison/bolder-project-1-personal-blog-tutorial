6e45f97e64050331d70c1fe1b489981497e1297b
Gitau Harrison <harrisongitau3@gmail.com>
2020-11-22 14:59:39 +0300

    Timestamps in flask

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import routes, models
